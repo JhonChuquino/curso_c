60edd121a3e87c1af66617c0df0449115cf06f43
Jhon Chuquino <jxxzz29@gmail.com>
2023-03-05 15:59:11 -0500

    progra

---
+++ areadelrectangulo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int area_rectangulo(int base, int altura)
+{
+	float area = 0;
+	area = (base*altura);
+	area = base*altura;
+	cout << "el area del triangulo es : " ; cout << area << "m^2" ;
+	return area;
+}
+int main()
+{
+	system("cls");
+	int area, base, altura;
+	cout << "ingrese la base y la altura " << endl;
+	cin >> base; cin >> altura;
+	return 0;
+}
